f014c196dfd5a3d85478bff01c3f5c7847998a94
Md Kayes Hossan Fuad <kayesthechill@gmail.com>
2023-09-11 20:41:33 +0600

    updated with guzzle HHTP

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -6,18 +6,11 @@ class FlightController extends Controller
 {
     public function flightsearch()
     {
-
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://gallpax.flyjatt.com/v1/Duffel/AirSearch.php',
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS =>'{
+        $client = new \GuzzleHttp\Client();
+        $headers = [
+            'Content-Type' => 'application/json',
+        ];
+        $body = '{
             "Segment": [
                 {
                     "DepFrom": "BKK",
@@ -30,19 +23,13 @@ class FlightController extends Controller
             "Child": 0,
             "Infant": 0,
             "Class": "Economy"
-        }',
-        CURLOPT_HTTPHEADER => array(
-            'Content-Type: application/json'
-        ),
-        ));
+        }';
 
-        $response = curl_exec($curl);
+        $request = new \GuzzleHttp\Psr7\Request('POST', 'https://gallpax.flyjatt.com/v1/Duffel/AirSearch.php', $headers, $body);
+        $res = $client->sendAsync($request)->wait();
+        $data = json_decode($res->getBody(), true);
 
-        curl_close($curl);
-        
-        $data =  json_decode($response, true);
 
         return view('flightsearch', ['data' => $data]);
-
     }
 }
